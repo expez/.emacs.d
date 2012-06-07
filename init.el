36cb0df7fb47b8148a033065251ed3cfe7be149e
Lars Andersen <mrgrandmother@gmail.com>
2012-06-07 10:42:55 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,30 @@
+;;package.el
+(require 'package)
+;; Set package archives.
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+                          ("gnu" . "http://elpa.gnu.org/packages/")
+                          ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(package-initialize)
+
+(defvar my-packages '(ace-jump-mode auctex bookmark+ dired+ dropdown-list ghc ghci-completion gist
+				    haskell-mode iy-go-to-char lisppaste magit magithub paredit
+				    pastebin parenface register-list session smex unbound undo-tree whole-line-or-region
+				    workgroups yasnippet)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-to-list 'load-path "~/.emacs.d/plugins")
+
+(require 'highlight-tags-mode)
+(require 'workgroups)
+(require 'yasnippet)
+(load-library "efuncs") ;; elisp functions.
+(load-library "my-config") ;; One-off variable settings.
+(load-library "bindings") ;; Keyboard bindings and aliases.
+(load-library "customize") ;;The stuff from customize lives in here.
---
+++ plugins/customize.el
@@ -0,0 +1,15 @@
+;;All the stuff from M-x customize goes in here.
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-font-list (quote ((98 "{\\bf " "}") (99 "{\\sc " "}") (101 "{\\em " "\\/}") (105 "{\\it " "\\/}") (114 "{\\rm " "}") (115 "{\\sl " "\\/}") (115 "{\\tt " "}") (100 "" "" t))))
+ '(auto-image-file-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
