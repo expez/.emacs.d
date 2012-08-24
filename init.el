a37c8cd310aa5235141c3d7c0ed99e4e97fc7d49
Expez <mrgrandmother@gmail.com>
2012-08-24 21:33:21 +0200

    Start using auto-complete again.
    Add a bunch of shit to use it for Haskell programming.
    Start using clang for c and integerate that with AC.

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
   (package-refresh-contents))
 (package-initialize)
 
-(defvar my-packages '(ace-jump-mode auctex bookmark+ dired+ dropdown-list ghc ghci-completion gist
+(defvar my-packages '(ace-jump-mode auto-complete auctex bookmark+ dired+ dropdown-list ghc ghci-completion gist
 				    expand-region git-commit lisppaste key-chord lorem-ipsum magit magithub move-text paredit
 				    pastebin parenface register-list session unbound undo-tree whole-line-or-region workgroups yasnippet)
   "A list of packages to ensure are installed at launch.")
@@ -27,17 +27,21 @@
                 (normal-top-level-add-subdirs-to-load-path)))
              load-path)))
 
+(require 'auto-complete-clang-async)
+(require 'auto-complete-config)
+(require 'color-moccur)
+(require 'evil)
+(require 'ex-mode)
 (require 'highlight-tags-mode)
+(require 'hpaste)
+(require 'ido-hacks)
+(require 'magit)
+(require 'parenface)
+(require 'relative-linum)
+(require 'surround)
 (require 'workgroups)
 (require 'yasnippet)
-(require 'parenface)
-(require 'hpaste)
-(require 'evil)
-(require 'surround)
-(require 'relative-linum)
-(require 'magit)
-(require 'ido-hacks)
-(require 'ex-mode)
+
 (load-library "my-config") ;; One-off variable settings.
 (load-library "customize") ;;The stuff from customize lives in here.
 
