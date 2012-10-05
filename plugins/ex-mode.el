5bf718c6b5af35661820dc503b9b407dd4cd0290
Expez <mrgrandmother@gmail.com>
2012-10-05 18:46:47 +0200

    Add ido-goto-symbol

--- plugins/ex-mode.el
+++ plugins/ex-mode.el
@@ -527,6 +527,54 @@ the current position of point, then move it to the beginning of the line."
        (find-file-noselect dir)
        (open-all-files-with-extension dir extension))))
 
+  (defun ido-goto-symbol (&optional symbol-list)
+      "Refresh imenu and jump to a place in the buffer using Ido."
+      (interactive)
+      (unless (featurep 'imenu)
+        (require 'imenu nil t))
+      (cond
+       ((not symbol-list)
+        (let ((ido-mode ido-mode)
+              (ido-enable-flex-matching
+               (if (boundp 'ido-enable-flex-matching)
+                   ido-enable-flex-matching t))
+              name-and-pos symbol-names position)
+          (unless ido-mode
+            (ido-mode 1)
+            (setq ido-enable-flex-matching t))
+          (while (progn
+                   (imenu--cleanup)
+                   (setq imenu--index-alist nil)
+                   (ido-goto-symbol (imenu--make-index-alist))
+                   (setq selected-symbol
+                         (ido-completing-read "Symbol? " symbol-names))
+                   (string= (car imenu--rescan-item) selected-symbol)))
+          (unless (and (boundp 'mark-active) mark-active)
+            (push-mark nil t nil))
+          (setq position (cdr (assoc selected-symbol name-and-pos)))
+          (cond
+           ((overlayp position)
+            (goto-char (overlay-start position)))
+           (t
+            (goto-char position)))))
+       ((listp symbol-list)
+        (dolist (symbol symbol-list)
+          (let (name position)
+            (cond
+             ((and (listp symbol) (imenu--subalist-p symbol))
+              (ido-goto-symbol symbol))
+             ((listp symbol)
+              (setq name (car symbol))
+              (setq position (cdr symbol)))
+             ((stringp symbol)
+              (setq name symbol)
+              (setq position
+                    (get-text-property 1 'org-imenu-marker symbol))))
+            (unless (or (null position) (null name)
+                        (string= (car imenu--rescan-item) name))
+              (add-to-list 'symbol-names name)
+              (add-to-list 'name-and-pos (cons name position))))))))
+
 (defvar ex-mode-keymap
   (let ((map (make-sparse-keymap)))
 
@@ -588,17 +636,18 @@ the current position of point, then move it to the beginning of the line."
     (define-key map (kbd "M-g M-c") 'goto-char)
     (define-key map (kbd "M-m") 'er/expand-region)
     (define-key map (kbd "<f5>") 'call-last-kbd-macro)
-    ;; Windmove
+    (define-key map (kbd "C-ci") 'ido-goto-symbol)
 
+    ;; Windmove
     (define-key map (kbd "<left>") 'windmove-left)
     (define-key map (kbd "<up>") 'windmove-up)
     (define-key map (kbd "<right>") 'windmove-right)
     (define-key map (kbd "<down>") 'windmove-down)
+
     (define-key map (kbd "C-x C-r")'ido-recentf-open)
     (define-key map (kbd "M-<up>") 'move-text-up)
     (define-key map (kbd "M-<down>") 'move-text-down)
     (define-key map (kbd "M-<backspace>") 'delete-indentation)
-
 
     map)
   "Keymap containing all my bindings. ")
