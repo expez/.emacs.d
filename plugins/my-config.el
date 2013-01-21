abeaeb42db00abac623d71a4fd0ad00fb4a7f6f0
Expez <mrgrandmother@gmail.com>
2013-01-21 21:33:53 +0100

    fix formatting

--- plugins/my-config.el
+++ plugins/my-config.el
@@ -201,33 +201,33 @@
 ;;Taken from emacswiki/ediffmode
 (defvar my-ediff-bwin-config nil "Window configuration before ediff.")
 (defcustom my-ediff-bwin-reg ?b
-"*Register to be set up to hold `my-ediff-bwin-config'
+  "*Register to be set up to hold `my-ediff-bwin-config'
 configuration.")
 
 (defvar my-ediff-awin-config nil "Window configuration after ediff.")
 (defcustom my-ediff-awin-reg ?e
-"*Register to be used to hold `my-ediff-awin-config' window
+  "*Register to be used to hold `my-ediff-awin-config' window
 configuration.")
 
 (defun my-ediff-bsh ()
-"Function to be called before any buffers or window setup for
+  "Function to be called before any buffers or window setup for
 ediff."
-(setq my-ediff-bwin-config (current-window-configuration))
-(when (characterp my-ediff-bwin-reg)
-(set-register my-ediff-bwin-reg
-(list my-ediff-bwin-config (point-marker)))))
+  (setq my-ediff-bwin-config (current-window-configuration))
+  (when (characterp my-ediff-bwin-reg)
+    (set-register my-ediff-bwin-reg
+                  (list my-ediff-bwin-config (point-marker)))))
 
 (defun my-ediff-ash ()
-"Function to be called after buffers and window setup for ediff."
-(setq my-ediff-awin-config (current-window-configuration))
-(when (characterp my-ediff-awin-reg)
-(set-register my-ediff-awin-reg
-(list my-ediff-awin-config (point-marker)))))
+  "Function to be called after buffers and window setup for ediff."
+  (setq my-ediff-awin-config (current-window-configuration))
+  (when (characterp my-ediff-awin-reg)
+    (set-register my-ediff-awin-reg
+                  (list my-ediff-awin-config (point-marker)))))
 
 (defun my-ediff-qh ()
-"Function to be called when ediff quits."
-(when my-ediff-bwin-config
-(set-window-configuration my-ediff-bwin-config)))
+  "Function to be called when ediff quits."
+  (when my-ediff-bwin-config
+    (set-window-configuration my-ediff-bwin-config)))
 
 (add-hook 'ediff-before-setup-hook 'my-ediff-bsh)
 (add-hook 'ediff-after-setup-windows-hook 'my-ediff-ash 'append)
