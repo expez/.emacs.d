a177fb08fdbf18a63737a5518283dff87be1c6f3
Expez <mrgrandmother@gmail.com>
2013-03-07 14:46:09 +0100

    Refactor ac setup a little

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -4,29 +4,26 @@
 
 (add-to-list 'ac-dictionary-directories (concat user-emacs-directory "ac-dict"))
 (ac-config-default)
+(setq ac-auto-start 2
+      ac-quick-help-delay 0.5
+      ac-fuzzy-enable t
+      ac-auto-show-menu 0.2)
 
-(defun my-ac-config ()
-  (add-hook 'LaTeX-mode-hook #'ac-l-setup)
-  (setq ac-auto-start 2)
-  (setq ac-quick-help-delay 0.5)
-  (setq ac-auto-show-menu 0.2)
-  (setq ac-fuzzy-enable t)
-  (add-hook 'css-mode-hook 'ac-css-mode-setup)
-  (ac-flyspell-workaround))
-(my-ac-config)
+(add-hook 'LaTeX-mode-hook #'ac-l-setup)
+(add-hook 'css-mode-hook 'ac-css-mode-setup)
+(ac-flyspell-workaround)
 
 (defun ac-exit-to-normal-state
   "Stops completing and returns to normal state"
   (interactive "")
-  (ac-abort)
+  (ac-stop)
   (evil-force-normal-state))
 
 (fill-keymap ac-completing-map
-             "C-[" 'ac-exit-to-normal-state)
-
-(fill-keymap ac-complete-mode-map
+             "C-[" 'ac-exit-to-normal-state
              "C-l" 'ac-expand-common
              "C-j" 'ac-next
+             "C-s" 'ac-isearch
              "C-k" 'ac-previous
              "ESC" 'ac-stop)
 
