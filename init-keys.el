d282485aabdce2643991e7d81ac5e3f7741d490e
Expez <mrgrandmother@gmail.com>
2013-03-18 08:06:27 +0100

    More keybindings.

--- init-keys.el
+++ init-keys.el
@@ -1,3 +1,12 @@
+(defkeymap misc-map
+  "w" 'toggle-whitespace
+  "g" 'gist-region-or-buffer
+  "r" 'rename-file-and-buffer
+  "d" 'delete-current-file
+  "p" 'pretty-mode
+  "d" 'diff-current-buffer-with-file
+  "v" 'visual-line-mode)
+
 (fill-keymap evil-normal-state-map
              "Y"     (kbd "y$")
              "U" 'universal-argument
@@ -17,7 +26,8 @@
              "M-p" 'previous-errror
              "<down>" 'move-text-down
              "<up>" 'move-text-up
-             "C-u" 'evil-scroll-up)
+             "C-u" 'evil-scroll-up
+             "K" misc-map)
 
 (fill-keymap evil-insert-state-map
              "C-y" 'yank
@@ -30,28 +40,37 @@
 
 (fill-keymaps (list evil-operator-state-map
                     evil-visual-state-map)
-              "SPC" 'evil-ace-jump-char-to-mode ;; works like `t'
-              "C-SPC" 'evil-ace-jump-char-mode ;; works like `f'
+              "SPC" 'evil-ace-jump-char-to-mode ; works like `t'
+              "C-SPC" 'evil-ace-jump-char-mode ; works like `f'
               "S-SPC" 'evil-ace-jump-line-mode)
 
 (fill-keymap evil-motion-state-map
              "C-e" 'move-end-of-line
              "C-a" 'smart-line-beginning)
 
+(evil-define-key 'normal markdown-mode
+  "j" 'evil-next-visual-line
+  "k" 'evil-previous-visual-line)
+
+(evil-define-key 'normal tex-mode
+  "j" 'evil-next-visual-line
+  "k" 'evil-previous-visual-line)
+
 (evil-leader/set-key
   "." 'evil-ex
-  "D" 'diff-buffer-with-file
   "K" 'kill-buffer-and-window
   "W" 'save-some-buffers
   "a" 'align-rexep
   "p" 'eproject-revisit-project
   "b" 'eproject-switch-to-buffer
+  "B" 'eproject-kill-project-buffers
   "c" 'compile
   "d" 'dired-jump
   "f" 'eproject-find-file
   "g" 'magit-status
   "k" 'kill-buffer
   "w" 'save-buffer
+  "s" 'eshell-toggle
   "v" 'vc-annotate
   "u" 'winner-undo)
 
@@ -61,13 +80,12 @@
              "C-x c" 'compile
              "C-x C-c" 'compile
              "C-x s" 'eshell-toggle
+             "C-x F" 'find-file-as-root
              "C-x C-b" 'ibuffer
              "C-x r v" 'register-list
              "C-<tab>" 'hippie-expand
-             ;; easy spell check
              "<f8>" 'ispell-word
               "<end>" 'sr-speedbar-toggle
-             ;;"C-S-<f8>" 'fd-switch-dictionary
              "M-S-<f8>" 'flyspell-buffer
              "C-<f8>" 'flyspell-check-previous-highlighted-word
              "M-<f8>" 'flyspell-check-next-highlighted-word
@@ -87,7 +105,6 @@
 (defalias 'mbf 'move-buffer-file)
 (defalias 'rb 'revert-buffer)
 (defalias 'oaf 'open-all-files-with-extension)
-(defalias 'dbf 'diff-buffer-with-file)
 
 (defalias 'gl 'gist-list)
 (defalias 'grb 'gist-region-or-buffer)
