4f83af19ea192587447fed17127c5b2e46eb45c4
Expez <mrgrandmother@gmail.com>
2013-03-21 02:54:37 +0100

    Bsd and mit license snippets.

--- init-yas.el
+++ init-yas.el
@@ -1,11 +1,10 @@
 (require 'yasnippet)
 
 (yas-global-mode 1)
-(setq yas-trigger-key nil)
-(yas/reload-all) ;; Needed to disable trigger key
+(yas/reload-all)
 (setq yas/root-directory  (concat user-emacs-directory "snippets"))
 (yas/load-directory yas/root-directory)
-(setq yas/prompt-functions '(yas/dropdown-prompt
+(setq yas-prompt-functions '(yas/dropdown-prompt
                              yas/ido-prompt
                              yas/completing-prompt))
 
