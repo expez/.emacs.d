93ad12e66fbbfd1dac35b1b438685e23c7344016
expez <expez@expez.com>
2013-04-02 14:16:44 +0200

    Update ack-and-a-half settings.

--- init-ack-and-a-half.el
+++ init-ack-and-a-half.el
@@ -1,14 +1,14 @@
-(let (ag (path-to-executable "ag"))
-  (if ag
-      (setq ack-and-a-half-executable ag)
-    (setq ack-and-a-half-executable "/usr/bin/vendor_perl/ack")))
+(let ((ag (path-to-executable "ag")))
+  (when ag
+      (setq ack-and-a-half-executable ag)))
 
 (setq ack-and-a-half-prompt-for-directory t)
 
 (defadvice ack-and-a-half-arguments-from-options (after
                                                   compatible-ag-options
                                                   activate)
-  (delete "--env" ad-return-value)
-  (delete "--noenv" ad-return-value))
+  (setq ad-return-value (delete "--env" ad-return-value))
+  (setq ad-return-value (delete "--noenv" ad-return-value))
+  (setq ad-return-value (delete "--nocolor" ad-return-value)))
 
 (provide 'init-ack-and-a-half)
