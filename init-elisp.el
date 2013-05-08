f8043f0ff990086ba2f224529b9d001bdd3fb8f4
expez <expez@expez.com>
2013-05-08 11:40:51 +0200

    Remove highlight-cl-changes.

--- init-elisp.el
+++ init-elisp.el
@@ -1,18 +1,18 @@
 (require 'parenface)
 (require 'pretty-mode-plus)
 (require 'elisp-slime-nav)
-(require 'highlight-cl)
+;(require 'highlight-cl)
 
 (set-face-foreground 'paren-face "grey30")
 
-(add-hook 'lisp-interaction-mode-hook 'highlight-cl-add-font-lock-keywords)
+;(add-hook 'lisp-interaction-mode-hook 'highlight-cl-add-font-lock-keywords)
 
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (flycheck-mode 0)
             (elisp-slime-nav-mode 1)
             (turn-on-redshank-mode)
-            (highlight-cl-add-font-lock-keywords)
+            ;(highlight-cl-add-font-lock-keywords)
             (esk-remove-elc-on-save)
             (checkdoc-minor-mode)
             (ac-emacs-lisp-mode-setup)
