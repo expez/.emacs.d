62f0fe559f043d5b3ac1244df0e7af1fd6a035b9
expez <expez@expez.com>
2013-05-27 12:37:52 +0200

    Fill-column 120 in latex mode.

--- init-latex.el
+++ init-latex.el
@@ -6,20 +6,21 @@
   (flyspell-mode 1)
   (LaTeX-math-mode 1)
   (turn-on-reftex)
+  (setq fill-column 120)
   (auto-complete-mode 1)
   (setq-default TeX-command-default "Build"
                 text-master 'dwim)
   (TeX-source-correlate-mode 1)
   (setq TeX-autosave t
-      TeX-save-query nil
-      TeX-parse-self t
-      TeX-PDF-mode t
-      reftex-plug-into-AUCTeX t
-      TeX-view-program-selection '((output-pdf "Evince"))
-      TeX-view-program-list '(("Evince" "evince --page-index=%(outpage) %o"))
-      TeX-source-correlate-start-server t
-      TeX-newline-function #'reindent-then-newline-and-indent
-      refTeX-plug-into-AUCTeX t)
+        TeX-save-query nil
+        TeX-parse-self t
+        TeX-PDF-mode t
+        reftex-plug-into-AUCTeX t
+        TeX-view-program-selection '((output-pdf "Evince"))
+        TeX-view-program-list '(("Evince" "evince --page-index=%(outpage) %o"))
+        TeX-source-correlate-start-server t
+        TeX-newline-function #'reindent-then-newline-and-indent
+        refTeX-plug-into-AUCTeX t)
   (turn-on-orgtbl))
 
 (add-hook 'LaTeX-mode-hook 'my-LaTeX-mode-hook)
