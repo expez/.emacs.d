b77ad577a64bec53802b99db753068736da3b8ce
expez <expez@expez.com>
2013-05-30 20:56:51 +0200

    Fix markdown keybindings.

--- init-markdown.el
+++ init-markdown.el
@@ -1,11 +1,12 @@
 (require 'markdown-mode)
 
-(fill-keymap markdown-mode-map
-             "C-c C-f C-b" 'markdown-insert-bold
-             "C-c C-f C-<tab>" 'markdown-insert-italic
-             "C-c C-f C-l" 'markdown-insert-link)
-
 (add-auto-mode 'markdown-mode "\\.md\\'")
+
+(fill-keymap markdown-mode-map
+               "C-c C-f" 'nil
+               "C-c C-f C-b" 'markdown-insert-bold
+               "C-c C-f C-<tab>" 'markdown-insert-italic
+               "C-c C-f C-l" 'markdown-insert-link)
 
 (add-lambda 'markdown-mode-hook
   (visual-line-mode 1))
