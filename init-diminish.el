b1363a9a0e8d3cca0fc09b47946459434ff9fa94
expez <expez@expez.com>
2013-05-30 20:48:52 +0200

    Cleanup the modeline a little.

--- init-diminish.el
+++ init-diminish.el
@@ -42,4 +42,12 @@
 (eval-after-load "whitespace"
   '(diminish 'whitespace-mode))
 
+(defmacro rename-modeline (package-name mode new-name)
+  `(eval-after-load ,package-name
+     (defadvice ,mode (after rename-modeline activate)
+        (setq mode-name ,new-name))))
+
+(rename-modeline "js2-mode" js2-mode "JS2")
+(rename-modeline "clojure-mode" clojure-mode "Clj")
+
 (provide 'init-diminish)
