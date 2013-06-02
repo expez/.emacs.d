24b7606957d3b8483ee4c07160330ee4a8ee0658
expez <expez@expez.com>
2013-06-02 21:47:44 +0200

    Remove ace-jump config, it's moved to evil-integration

--- init-evil.el
+++ init-evil.el
@@ -16,44 +16,6 @@
 
 (evil-ex-define-cmd "n[ew]" 'evil-window-new)
 
-(defmacro evil-enclose-ace-jump (&rest body)
-  `(let ((old-mark (mark)))
-     (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
-     (remove-hook 'post-command-hook #'evil-visual-post-command t)
-     (unwind-protect
-         (progn
-           ,@body
-           (recursive-edit))
-       (if (evil-visual-state-p)
-           (progn
-             (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
-             (add-hook 'post-command-hook #'evil-visual-post-command nil t)
-             (set-mark old-mark))
-         (push-mark old-mark)))))
-
-(evil-define-motion evil-ace-jump-char-mode (count)
-  :type exclusive
-  (evil-enclose-ace-jump
-   (ace-jump-mode 5)))
-
-(evil-define-motion evil-ace-jump-line-mode (count)
-  :type line
-  (evil-enclose-ace-jump
-   (ace-jump-mode 9)))
-
-(evil-define-motion evil-ace-jump-word-mode (count)
-  :type exclusive
-  (evil-enclose-ace-jump
-   (ace-jump-mode 1)))
-
-(evil-define-motion evil-ace-jump-char-to-mode (count)
-  :type exclusive
-  (evil-enclose-ace-jump
-   (ace-jump-mode 5)
-   (forward-char -1)))
-
-(add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
-
 (defadvice evil-goto-definition (around evil-clever-goto-def activate)
   "Make use of emacs', slime's and etags possibilities for finding definitions."
   (case major-mode
