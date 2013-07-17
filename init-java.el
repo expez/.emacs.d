cf6367877e6b4c3eb97bb138847f26e82f0b29db
expez <expez@expez.com>
2013-07-17 13:21:55 +0200

    update java config

--- init-java.el
+++ init-java.el
@@ -4,7 +4,7 @@
 (require 'java-mode-indent-annotations)
 
 (setq eclimd-default-workspace "~/workspace"
-      eclim-executable "/usr/share/eclipse/eclim"
+      eclim-executable "/usr/share/eclipse/plugins/org.eclim_2.2.7.4-ge53b9ed/bin/eclim"
       eclim-eclipse-dirs '("/usr/share/eclipse")
       eclim-auto-save t
       help-at-pt-display-when-idle t
@@ -25,11 +25,44 @@
   (ac-emacs-eclim-java-setup)
   (help-at-pt-set-timer)
   (java-mode-indent-annotations-setup)
+  (define-key evil-normal-state-local-map (kbd "M-.")
+    'eclim-java-find-declaration)
   (when eclim-mode
     (ac-emacs-eclim-java-setup)))
 
 (fill-keymap eclim-mode-map
-             "C-c C-e C-d" 'eclim-java-show-documentation-for-current-element)
+             "C-c C-e C-d" 'eclim-java-show-documentation-for-current-element
+             "C-c d" 'eclim-java-show-documentation-for-current-element
+             "C-c t" 'eclim-java-find-type
+             "C-c f" 'eclim-java-find-generic
+             "C-c r" 'eclim-java-find-references
+             "C-c s" 'eclim-goto-super)
+
+(defun eclim-goto-super ()
+  "Jump to the superclass declaration."
+  (interactive)
+  (save-excursion
+    (re-search-backward "extends \\(\\w+\\)" nil t)
+    (forward-word)
+    (forward-char)
+    (eclim-java-find-type (match-string 1))))
+
+;;; This version does not visit file in another buffer
+(defun eclim--visit-declaration (line)
+  (ring-insert find-tag-marker-ring (point-marker))
+  (find-file (assoc-default 'filename line))
+  (goto-line (assoc-default 'line line))
+  (move-to-column (- (assoc-default 'column line) 1)))
+
+(add-lambda 'eclim-problems-mode-hook
+  (define-key evil-normal-state-local-map (kbd "a") 'eclim-problems-show-all)
+  (define-key evil-normal-state-local-map (kbd "e") 'eclim-problems-show-errors)
+  (define-key evil-normal-state-local-map (kbd "g") 'eclim-problems-buffer-refresh)
+  (define-key evil-normal-state-local-map (kbd "q") 'eclim-quit-window)
+  (define-key evil-normal-state-local-map (kbd "w") 'eclim-problems-show-warnings)
+  (define-key evil-normal-state-local-map (kbd "f") 'eclim-problems-toggle-filefilter)
+  (define-key evil-normal-state-local-map (kbd "c") 'eclim-problems-correct)
+  (define-key evil-normal-state-local-map (kbd "RET") 'eclim-problems-open-current))
 
 (defun ant-compile ()
   "Traveling up the path, find build.xml file and run compile."
@@ -55,8 +88,7 @@ then it'll call “java x” in a shell."
 (defun eclim-ac-complete ()
   "Query eclim for available completions at point."
   (interactive)
-  (auto-complete (list 'ac-source-emacs-eclim
-                       'ac-source-emacs-eclim-c-dot)))
+  (auto-complete (list ac-source-emacs-eclim)))
 
 (defadvice ethan-wspace-clean-before-save-hook (around eclim-compat activate)
   "Fix spaces being removed while typing because eclim autosaves
