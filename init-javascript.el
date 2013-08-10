90922cff94b20ba6be2b2d10a45b7541cc410840
expez <expez@expez.com>
2013-08-10 13:59:59 +0200

    Add init-javascript

---
+++ init-javascript.el
@@ -0,0 +1,153 @@
+(require 'js2-mode)
+(require 'js2-imenu-extras)
+(require 'skewer-mode)
+(require 'skewer-repl)
+(require 'skewer-html)
+(require 'skewer-css)
+(require 'js2-refactor)
+(require 'tern)
+(require 'tern-auto-complete)
+
+;;; bookmarklet to load skewer:
+;;; javascript:(function(){var d=document;var s=d.createElement('script');s.src='http://localhost:8023/skewer';d.body.appendChild(s);})()
+
+(defun skewer-start ()
+  (interactive)
+  (let ((httpd-port 8023)
+        (browse-url-generic-program "chromium"))
+    (httpd-start)
+    (message "Ready to skewer the browser. Now jack in with the bookmarklet.")))
+
+(defun skewer-demo ()
+  (interactive)
+  (let ((httpd-port 8024))
+    (run-skewer)
+    (skewer-repl)))
+
+(tern-ac-setup)
+(add-auto-mode 'js2-mode "\\.js")
+
+(add-hook 'js2-mode-hook
+          '(lambda ()
+             (setq mode-name "JS2")
+             (skewer-mode)
+             (tern-mode t)))
+
+(js2-imenu-extras-setup)
+
+(setq-default js2-use-font-lock-faces t
+              js2-indent-on-enter-key nil
+              js2-auto-indent-p t
+              js2-bounce-indent-p nil
+              js2-concat-multiline-strings 'eol
+              js2-idle-timer-delay 0.1
+              js2-highlight-level 3
+              js2-mode-show-parse-errors nil
+              js2-strict-missing-semi-warning nil
+              js2-strict-inconsistent-return-warning nil
+              js2-strict-trailing-comma-warning t)
+
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
+;; js2-mode steals TAB, let's steal it back for yasnippet
+(defun js2-tab-properly ()
+  (interactive)
+  (let ((yas/fallback-behavior 'return-nil))
+    (unless (yas/expand)
+      (indent-for-tab-command)
+      (if (looking-back "^\s*")
+          (back-to-indentation)))))
+
+(define-key js2-mode-map (kbd "TAB") 'js2-tab-properly)
+
+;; Use lambda for anonymous functions
+(font-lock-add-keywords
+ 'js2-mode `(("\\(function\\) *("
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "\u0192")
+                        nil)))))
+
+(defun my-aget (key map)
+  (cdr (assoc key map)))
+
+(defun js2-fetch-autolint-externs (file)
+  (let* ((settings (with-temp-buffer
+                     (insert-file-literally file)
+                     (javascript-mode)
+                     (let (kill-ring) (kill-comment 1000))
+                     (->> (buffer-substring (point-min) (point-max))
+                       (s-trim)
+                       (s-chop-prefix "module.exports = ")
+                       (s-chop-suffix ";")
+                       (json-read-from-string))))
+         (predef (->> settings
+                   (my-aget 'linterOptions)
+                   (my-aget 'predef))))
+    (--each (append predef nil)
+      (add-to-list 'js2-additional-externs it))))
+
+;; After js2 has parsed a js file, we look for jslint globals decl comment ("/* global Fred, _, Harry */") and
+;; add any symbols to a buffer-local var of acceptable global vars
+;; Note that we also support the "symbol: true" way of specifying names via a hack (remove any ":true"
+;; to make it look like a plain decl, and any ':false' are left behind so they'll effectively be ignored as
+;; you can;t have a symbol called "someName:false"
+(add-hook 'js2-post-parse-callbacks
+          (lambda ()
+            (when (> (buffer-size) 0)
+              (let ((btext (replace-regexp-in-string
+                            ": *true" " "
+                            (replace-regexp-in-string "[\n\t ]+" " " (buffer-substring-no-properties 1 (buffer-size)) t t))))
+                (mapc (apply-partially 'add-to-list 'js2-additional-externs)
+                      (split-string
+                       (if (string-match "/\\* *global *\\(.*?\\) *\\*/" btext) (match-string-no-properties 1 btext) "")
+                       " *, *" t))
+                ))))
+
+(defun cjsp--eldoc-innards (beg)
+  (save-excursion
+    (goto-char beg)
+    (search-forward "=")
+    (let ((start (point)))
+      (search-forward "*/")
+      (forward-char -2)
+      (buffer-substring-no-properties start (point)))))
+
+(defun cjsp--indentation-of-html-line (html line-number)
+  (with-temp-buffer
+    (insert html)
+    (html-mode)
+    (indent-region (point-min) (point-max))
+    (goto-line line-number)
+    (back-to-indentation)
+    (current-column)))
+
+(defun cjsp--line-number-in-eldoc (p beg)
+  (save-excursion
+    (goto-char p)
+    (let ((l (line-number-at-pos)))
+      (goto-char beg)
+      (- l (line-number-at-pos) -1))))
+
+(defun js2-lineup-comment (parse-status)
+  "Indent a multi-line block comment continuation line."
+  (let* ((beg (nth 8 parse-status))
+         (first-line (js2-same-line beg))
+         (p (point))
+         (offset (save-excursion
+                   (goto-char beg)
+                   (cond
+
+                    ((looking-at "/\\*:DOC ")
+                     (+ 2 (current-column)
+                        (cjsp--indentation-of-html-line
+                         (cjsp--eldoc-innards beg)
+                         (cjsp--line-number-in-eldoc p beg))))
+
+                    ((looking-at "/\\*")
+                     (+ 1 (current-column)))
+
+                    (:else 0)))))
+    (unless first-line
+      (indent-line-to offset))))
+
+(provide 'init-javascript)
