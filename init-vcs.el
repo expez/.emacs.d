89e4267f8d23796ddc304eabf78d77b7664278bb
expez <expez@expez.com>
2013-08-23 08:33:25 +0200

    vc-annotate in own window

--- init-vcs.el
+++ init-vcs.el
@@ -109,4 +109,19 @@
   (delete-current-buffer-file)
   (magit-refresh))
 
+(defun vc-annotate-quit ()
+  "Restores the previous window configuration and kills the vc-annotate buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :vc-annotate-fullscreen))
+
+(eval-after-load "vc-annotate"
+  '(progn
+     (defadvice vc-annotate (around fullscreen activate)
+       (window-configuration-to-register :vc-annotate-fullscreen)
+       ad-do-it
+       (delete-other-windows))
+
+     (define-key vc-annotate-mode-map (kbd "q") 'vc-annotate-quit)))
+
 (provide 'init-vcs)
