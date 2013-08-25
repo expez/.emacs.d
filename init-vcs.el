696934f91f0cffe24bf6154fff6964d081ea48e9
expez <expez@expez.com>
2013-08-25 09:41:57 +0200

    Fix popup behavior in new magit

--- init-vcs.el
+++ init-vcs.el
@@ -127,4 +127,17 @@
 
      (define-key vc-annotate-mode-map (kbd "q") 'vc-annotate-quit)))
 
+;; Add an extra newline to separate commit message from git commentary
+
+(defun magit-commit-mode-init ()
+  (when (looking-at "\n")
+    (open-line 1)))
+
+(add-hook 'git-commit-mode-hook 'magit-commit-mode-init)
+
+;; close popup when commiting
+
+(defadvice git-commit-commit (after delete-window activate)
+  (delete-window))
+
 (provide 'init-vcs)
