f821a95237b8792f237fddd00f495a5c4601b98c
expez <expez@expez.com>
2013-10-01 11:00:50 +0200

    Add autopairs, remove highlight-cl, swap hook lambda for fn

--- init-elisp.el
+++ init-elisp.el
@@ -2,24 +2,23 @@
 (require 'pretty-mode-plus)
 (require 'elisp-slime-nav)
 (require 'evil-paredit)
-;(require 'highlight-cl)
 
 (set-face-foreground 'paren-face "grey30")
 
-;(add-hook 'lisp-interaction-mode-hook 'highlight-cl-add-font-lock-keywords)
 
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (elisp-slime-nav-mode 1)
-            (turn-on-redshank-mode)
-            (flycheck-mode 0)
-          ; (highlight-cl-add-font-lock-keywords)
-            (esk-remove-elc-on-save)
-            (checkdoc-minor-mode)
-            (local-set-key (kbd "RET") 'newline-and-indent)
-            (ac-emacs-lisp-mode-setup)
-            (define-key evil-normal-state-local-map (kbd "M-.")
-              'elisp-slime-nav-find-elisp-thing-at-point)))
+(defun my-elisp-mode-hook ()
+  (elisp-slime-nav-mode 1)
+  (turn-on-redshank-mode)
+  (flycheck-mode 0)
+  (esk-remove-elc-on-save)
+  (checkdoc-minor-mode)
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  (ac-emacs-lisp-mode-setup)
+  (push '(?` . ("`" . "'")) surround-pairs-alist)
+  (define-key evil-normal-state-local-map (kbd "M-.")
+    'elisp-slime-nav-find-elisp-thing-at-point))
+
+(add-hook 'emacs-lisp-mode-hook #'my-elisp-mode-hook)
 
 (defun ielm-auto-complete ()
   "Enables `auto-complete' support in \\[ielm]."
@@ -33,15 +32,15 @@
 (add-hook 'ielm-mode-hook 'ielm-auto-complete)
 
 (let ((elisp-programming-major-modes '(emacs-lisp-mode lisp-interaction-mode
-                                                      ielm-mode)))
+                                                       ielm-mode)))
   (dolist (mode elisp-programming-major-modes)
-          (add-hook
-           (intern (concat (symbol-name mode) "-hook"))
-           (lambda ()
-             (turn-on-eldoc-mode)
-             (paredit-mode 1)
-             (evil-paredit-mode 1)
-             (rainbow-delimiters-mode 0)))))
+    (add-hook
+     (intern (concat (symbol-name mode) "-hook"))
+     (lambda ()
+       (turn-on-eldoc-mode)
+       (paredit-mode 1)
+       (evil-paredit-mode 1)
+       (rainbow-delimiters-mode 0)))))
 
 (defun esk-remove-elc-on-save ()
   "If you're saving an elisp file, likely the .elc is no longer valid."
