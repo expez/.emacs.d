3b69565e2e2817906cf903f1e86e2e7b4a613805
expez <expez@expez.com>
2013-10-28 09:54:50 +0100

    tagedit in nxml-mode

--- init-html.el
+++ init-html.el
@@ -1,6 +1,6 @@
 (require 'sgml-mode)
 (require 'know-your-http-well)
-
+(require 'nxml-mode)
 (add-hook 'html-mode-hook 'skewer-html-mode)
 (add-hook 'html-mode-hook 'rainbow-mode)
 
@@ -19,17 +19,18 @@
 
 (add-hook 'sgml-mode-hook 'zencoding-mode)
 
-(fill-keymap html-mode-map
-             (kbd "C-c C-w") 'html-wrap-in-tag
-             (kbd "C-)") 'tagedit-forward-slurp-tag
-             (kbd "M-)") 'tagedit-forward-barf-tag
-             (kbd "M-r") 'tagedit-raise-tag
-             (kbd "M-s") 'tagedit-splice-tag
-             (kbd "M-S") 'tagedit-split-tag
-             (kbd "M-j") 'tagedit-join-tags
-             (kbd "M-?") 'tagedit-convolute-tags
-             (kbd "M-k") 'tagedit-kill-attribute
-             (kbd "M-<return>") 'tagedit-toggle-multiline-tag)
+(fill-keymaps '(html-mode-map nxml-mode-map)
+              (kbd "C-c C-w") 'html-wrap-in-tag
+              (kbd "C-)") 'tagedit-forward-slurp-tag
+              (kbd "M-)") 'tagedit-forward-barf-tag
+              (kbd "M-r") 'tagedit-raise-tag
+              (kbd "M-s") 'tagedit-splice-tag
+              (kbd "M-S") 'tagedit-split-tag
+              (kbd "M-j") 'tagedit-join-tags
+              (kbd "M-?") 'tagedit-convolute-tags
+              (kbd "M-k") 'tagedit-kill
+              (kbd "M-K") 'tagedit-kill-attribute
+              (kbd "M-<return>") 'tagedit-toggle-multiline-tag)
 
 (tagedit-add-experimental-features)
 (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))
