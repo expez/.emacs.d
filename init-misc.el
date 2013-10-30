c4f9f9f1ed7d40e6e08a270551da5e24c5627f29
expez <expez@expez.com>
2013-10-30 21:03:33 +0100

    Run ping and traceroute in same manner as magit status

--- init-misc.el
+++ init-misc.el
@@ -105,3 +105,20 @@
       (setq httpd-root (ido-read-directory-name "Serve dir: " dir))
       ad-do-it)
     ad-do-it))
+
+(defun net-utils-restore-windows ()
+  "Restore windows and clean up after ping."
+  (interactive)
+  (kill-buffer (current-buffer))
+  (jump-to-register :net-utils-fullscreen))
+
+(defadvice net-utils-run-program (around net-utils-big-page activate)
+  (window-configuration-to-register :net-utils-fullscreen)
+  (let ((buf ad-do-it))
+    (switch-to-buffer buf)
+    (delete-other-windows)
+    (set-temporary-overlay-map
+      (let ((map (make-sparse-keymap)))
+        (define-key map (kbd "q") 'net-utils-restore-windows)
+        map))
+    (message "Type \"q\" to restore other windows.")))
