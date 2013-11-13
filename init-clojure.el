26d873e497a7e966b298ca51a0daaaee4475ff86
expez <expez@expez.com>
2013-11-13 14:01:44 +0100

    refactor clj config

--- init-clojure.el
+++ init-clojure.el
@@ -8,55 +8,52 @@
 (require 'elein)
 (require 'slamhound)
 
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook
-          'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-mode-hook
-          (lambda () (setq show-trailing-whitespace nil)))
+(eval-after-load 'cider
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
 
 (add-auto-mode 'clojure-mode "\\.cljs\\'")
 
 (eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
-
-(add-hook 'nrepl-repl-mode-hook 'paredit-mode)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
-(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+  '(add-to-list 'ac-modes 'cider-repl-mode))
 
 (defadvice clojure-test-run-tests (before save-first activate)
   (save-buffer))
 
-(defadvice nrepl-load-current-buffer (before save-first activate)
+(defadvice cider-load-current-buffer (before save-first activate)
   (save-buffer))
+
+(defun my-cider-mode-hook ()
+  (cider-turn-on-eldoc-mode)
+  (ac-nrepl-setup))
+(add-hook 'cider-mode-hook 'my-cider-mode-hook)
+
+(defun my-cider-repl-mode-hook ()
+  (setq show-trailing-whitespace nil)
+  (ac-nrepl-setup)
+  (paredit-mode 1))
+(add-hook 'cider-repl-mode-hook 'my-cider-repl-mode-hook)
 
 (defun my-clojure-mode-hook ()
   (rainbow-delimiters-mode 0)
   (clj-refactor-mode 1)
   (cider-mode 1)
+  (paredit-mode 1)
   (cljr-add-keybindings-with-prefix "C-c")
   (fill-keymap evil-normal-state-local-map
-                          "M-." 'cider-jump
-                          "M-," 'cider-jump-back
-                          "M-TAB" 'complete-symbol))
+               "M-." 'cider-jump
+               "M-," 'cider-jump-back
+               "M-TAB" 'complete-symbol))
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
-(define-key clojure-mode-map (kbd "s-j") 'clj-jump-to-other-file)
-
 (setq nrepl-hide-special-buffers t)
-(setq nrepl-popup-stacktraces-in-repl t)
-(setq nrepl-history-file "~/.emacs.d/nrepl-history")
+(setq cider-repl-popup-stacktraces t)
+(setq cider-repl-history-file "~/.emacs.d/nrepl-history")
 
 (add-hook 'nrepl-connected-hook
           (defun my-nrepl-eldoc-hook ()
             (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
-            (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+            (add-hook 'cider-repl-mode-hook 'cider-turn-on-eldoc-mode)
             (nrepl-enable-on-existing-clojure-buffers)))
 
 (put-clojure-indent 'match 1)
