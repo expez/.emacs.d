c535a571d9c19478794bf1e42fd9d290392de3a5
expez <expez@expez.com>
2014-03-12 19:06:35 +0100

    Remove unused util function

--- init-util.el
+++ init-util.el
@@ -158,33 +158,6 @@ For Mac OS X, use “'unix”."
   (interactive)
   (insert (format-time-string "%c" (current-time))))
 
-(defun toggle-letter-case ()
-  "Toggle the letter case of current word or text selection.
-Toggles between: “all lower”, “Init Caps”, “ALL CAPS”."
-  (interactive)
-  (let (p1 p2 (deactivate-mark nil) (case-fold-search nil))
-    (if (region-active-p)
-        (setq p1 (region-beginning) p2 (region-end))
-      (let ((bds (bounds-of-thing-at-point 'word)))
-        (setq p1 (car bds) p2 (cdr bds))))
-    (when (not (eq last-command this-command))
-      (save-excursion
-        (goto-char p1)
-        (cond
-         ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
-         ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps"))
-         ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps"))
-         ((looking-at "[[:lower:]]") (put this-command 'state "all lower"))
-         ((looking-at "[[:upper:]]") (put this-command 'state "all caps"))
-         (t (put this-command 'state "all lower")))))
-    (cond
-     ((string= "all lower" (get this-command 'state))
-      (upcase-initials-region p1 p2) (put this-command 'state "init caps"))
-     ((string= "init caps" (get this-command 'state))
-      (upcase-region p1 p2) (put this-command 'state "all caps"))
-     ((string= "all caps" (get this-command 'state))
-      (downcase-region p1 p2) (put this-command 'state "all lower")))))
-
 (defun how-many-in-region (begin end regexp &optional interactive)
   "Print number of non-trivial matches for REGEXP in region.
  Non-interactive arguments are Begin End Regexp"
@@ -254,7 +227,6 @@ This is to update existing buffers after a Git pull of their underlying files."
   "Show the full path file name in the minibuffer."
   (interactive)
   (message (buffer-file-name)))
-
 
 (defun copy-file-name-to-clipboard ()
   "Copy the current buffer file name to the clipboard."
