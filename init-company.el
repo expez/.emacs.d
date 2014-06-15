d18a3c90998b433d30eff1cfc76b64014e127112
expez <expez@expez.com>
2014-06-15 12:21:11 +0200

    More company settings

--- init-company.el
+++ init-company.el
@@ -1,10 +1,15 @@
 (require-package 'company)
 (require 'company)
+(require 'company-dabbrev-code)
 
 (setq company-idle-delay 0.2
       company-tooltip-limit 10
       company-minimum-prefix-length 2
+      company-selection-wrap-around t
+      company-selection-changed t
       company-require-match nil)
+
+(add-to-list 'company-dabbrev-code-modes 'clojure-mode)
 
 (fill-keymap company-active-map
              "C-h" 'company-show-doc-buffer
