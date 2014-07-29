2b771e102b49c831bd87f1aede852fb370fb4456
Lars Andersen <lars.andersen@gjensidige.no>
2014-07-29 13:42:12 +0200

    browse-ns-all => browse-ns

--- init-clojure.el
+++ init-clojure.el
@@ -66,7 +66,7 @@
                "C-c h" 'clojure-cheatsheet
                "C-c M-b" 'cider-browse-ns-all
                "C-c m" 'cider-macroexpand-1
-               "C-c M" 'cider-macroexpand-all)
+               "C-c M" 'cider-macroexpand)
   (cljr-add-keybindings-with-prefix "C-c C-m")
   (paredit-mode 1)
   (evil-paredit-mode 1)
