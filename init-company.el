70594392ce31a55ffaba6fed8180c33281432aee
expez <expez@expez.com>
2014-08-23 09:55:50 +0200

    Increase company min prefix to 2

--- init-company.el
+++ init-company.el
@@ -5,9 +5,10 @@
 (setq company-idle-delay 0.2
       company-show-numbers t
       company-tooltip-limit 10
-      company-minimum-prefix-length 1
+      company-minimum-prefix-length 2
       company-selection-wrap-around t
       company-selection-changed t
+      company-tooltip-flip-when-above t
       company-require-match nil)
 
 (add-to-list 'company-dabbrev-code-modes 'clojure-mode)
