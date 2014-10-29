21dd1dc8586f29a6db8aa8531b3f4e1332481254
expez <expez@expez.com>
2014-10-29 19:05:06 +0100

    update url for melpa

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,9 +1,8 @@
 (require 'package)
 
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/" )
-                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ;; ("gnu" . "http://elpa.gnu.org/packages/")
-))
+(setq package-archives '(("melpa" . "http://melpa.org/packages/" )
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
 
 (when (not package-archive-contents)
   (package-refresh-contents))
