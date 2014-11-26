ccf1a24249f8b702031702156c39900fff4ef442
Lars Andersen <lars.andersen@gjensidige.no>
2014-11-26 08:27:43 +0100

    customize seems to be pretty printing its stuff now

--- customize.el
+++ customize.el
@@ -3,25 +3,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-font-list (quote ((98 "{\\bf " "}") (99 "{\\sc " "}") (101 "{\\em " "\\/}") (105 "{\\it " "\\/}") (114 "{\\rm " "}") (115 "{\\sl " "\\/}") (115 "{\\tt " "}") (100 "" "" t))))
+ '(TeX-font-list
+   (quote
+    ((98 "{\\bf " "}")
+     (99 "{\\sc " "}")
+     (101 "{\\em " "\\/}")
+     (105 "{\\it " "\\/}")
+     (114 "{\\rm " "}")
+     (115 "{\\sl " "\\/}")
+     (115 "{\\tt " "}")
+     (100 "" "" t))))
  '(auto-image-file-mode t)
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(bmkp-last-as-first-bookmark-file "~\\.emacs.d\\bookmarks")
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(fringe-mode (cons 8 0) nil (fringe))
  '(glasses-face (quote bold))
  '(glasses-original-separator "")
  '(glasses-separate-capital-groups nil)
  '(glasses-separate-parentheses-p nil)
- '(glasses-separator "")
- '(mediawiki-site-alist (quote (("ExWiki" "http://expez.com/w/" "expez" "" "Main Page")))))
+ '(glasses-separator ""))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ein:cell-input-area ((t (:background "#073642"))) t)
+ '(ein:cell-input-area ((t (:background "#073642"))))
  '(ethan-wspace-face ((t (:foreground "#882C02" :inverse-video t :weight bold))))
  '(web-mode-builtin-face ((t (:inherit font-lock-builtin-face :foreground "font-lock-builtin-face"))))
  '(wg-divider-face ((t nil)))
