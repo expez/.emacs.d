b1ed19fa58a5928ac9c97e721f36436fe88a1a8f
expez <expez@expez.com>
2014-12-19 20:49:43 +0100

    add narrow-or-widen-dwim

--- lisp/init-util.el
+++ lisp/init-util.el
@@ -703,4 +703,19 @@ Then saves the buffer."
          (cd "build"))))
     (start-process "my-urxvt" nil "urxvtc")))
 
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
+Intelligently means: region, subtree, or defun, whichever applies
+first.
+
+With prefix P, don't widen, just narrow even if buffer is already
+narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
+        (t (narrow-to-defun))))
+
 (provide 'init-util)
