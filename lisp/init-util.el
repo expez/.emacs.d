ffb90e480aacbc990b704641a15777add1729b57
expez <expez@expez.com>
2014-12-20 10:09:32 +0100

    update edit-user-config-file

--- lisp/init-util.el
+++ lisp/init-util.el
@@ -636,10 +636,9 @@ indicate failure."
   "Use completing read to open one of the configuration files for
   emacs for editing."
   (interactive)
-  (let ((files (cons (concat user-emacs-directory "init.el")
-                     (directory-files my-config-dir t "init-.*?\.el\$"))))
+  (let ((files (directory-files my-config-dir t "init-.*?\.el\$")))
     (find-file (ido-read-file-name "Edit file: " my-config-dir nil
-                                 nil "init-"))))
+                                   nil "init-"))))
 
 (defun find-shell-init-file ()
   "Edit the shell init file in another window."
