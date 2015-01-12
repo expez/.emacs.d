23d0dbea98f4d4fc6efb2b6cea1147f248c4a8fb
Lars Andersen <lars.andersen@gjensidige.no>
2015-01-12 10:08:30 +0100

    restore shadowed neotree binding

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -208,6 +208,7 @@
               (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
               (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
               (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+              (define-key evil-normal-state-local-map (kbd "G") 'neotree-refresh)
               (define-key evil-normal-state-local-map (kbd "U") 'neotree-select-up-node)
               (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
 
