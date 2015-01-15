f52b8a93681fb29da027ba59af565f9ea87d2164
Lars Andersen <lars.andersen@gjensidige.no>
2015-01-15 09:00:33 +0100

    Get rid of tagedit in favor of smartparens

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,13 +1,10 @@
 (require-package 'emmet-mode)
 (require-package 'ac-emmet)
-(require-package 'tagedit)
 (require-package 'know-your-http-well)
 (require-package 'web-mode)
 (require-package 'angular-snippets)
 (require 'sgml-mode)
 (require 'know-your-http-well)
-(require 'nxml-mode)
-(require 'tagedit)
 (require 'web-mode)
 (require 'emmet-mode)
 (require 'angular-snippets)
@@ -29,8 +26,7 @@
 (defun my-html-mode-hook ()
   (skewer-html-mode 1)
   (setq-local tab-width 2)
-  (change-whitespace-line-column 120)
-  (tagedit-mode 1))
+  (change-whitespace-line-column 120))
 (add-hook 'html-mode-hook #'my-html-mode-hook)
 
 (add-hook 'sgml-mode-hook 'emmet-mode)
@@ -40,35 +36,6 @@
   '(define-key html-mode-map (kbd "C-c C-d") 'ng-snip-show-docs-at-point))
 
 (setq emmet-move-cursor-between-quotes t)
-
-(defun html-wrap-in-tag (beg end)
-  (interactive "r")
-  (let ((oneline? (= (line-number-at-pos beg) (line-number-at-pos end))))
-    (deactivate-mark)
-    (goto-char end)
-    (unless oneline? (newline-and-indent))
-    (insert "</div>")
-    (goto-char beg)
-    (insert "<div>")
-    (unless oneline? (newline-and-indent))
-    (indent-region beg (+ end 11))
-    (goto-char (+ beg 4))))
-
-(defun html-wrap-in-tag (tag &optional beg end)
-  "Add a tag to beginning and ending of current word or text selection."
-  (interactive "sEnter tag name: ")
-  (if (or (use-region-p)
-          (and beg end))
-      (progn
-        (setq beg (or beg (region-beginning)))
-        (setq end (or end (region-end))))
-    (let ((bds (bounds-of-thing-at-point 'symbol)))
-      (setq beg (car bds))
-      (setq end (cdr bds))))
-  (goto-char end)
-  (insert "</" tag ">")
-  (goto-char beg)
-  (insert "<" tag ">"))
 
 (defun html-create-list ()
   "Creates a unordered list from the lines in REGION.
@@ -94,19 +61,6 @@ With a prefix it creates an ordered list."
 
 (fill-keymaps '(html-mode-map nxml-mode-map web-mode-map)
               (kbd "RET") 'newline-and-indent
-              (kbd "C-c C-w") 'html-wrap-in-tag
-              (kbd "C-)") 'tagedit-forward-slurp-tag
-              (kbd "M-)") 'tagedit-forward-barf-tag
-              (kbd "M-r") 'tagedit-raise-tag
-              (kbd "M-s") 'tagedit-splice-tag
-              (kbd "M-S") 'tagedit-split-tag
-              (kbd "M-j") 'tagedit-join-tags
-              (kbd "M-?") 'tagedit-convolute-tags
-              (kbd "M-k") 'tagedit-kill
-              (kbd "M-K") 'tagedit-kill-attribute
-              (kbd "M-<return>") 'tagedit-toggle-multiline-tag
-              "M-l" 'sgml-skip-tag-forward
-              "M-h" 'sgml-skip-tag-backward
               "C-c l" 'html-create-list)
 
 (defadvice sgml-delete-tag (after reindent activate)
