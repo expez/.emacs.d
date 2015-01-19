99d5cb64fe35d40796da5b14bdf0258c43164d28
Lars Andersen <lars.andersen@gjensidige.no>
2015-01-19 08:58:00 +0100

    projectile, use `find` also on windows

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -2,8 +2,8 @@
 
 (projectile-global-mode)
 
-(setq projectile-use-git-grep t)
-
-(setq projectile-globally-ignored-directories (append '("bower_components")
-                                                      projectile-globally-ignored-directories))
+(setq projectile-use-git-grep t
+      projectile-indexing-method 'alien)
+(setq projectile-globally-ignored-directories
+      (append '("bower_components") projectile-globally-ignored-directories))
 (provide 'init-projectile)
