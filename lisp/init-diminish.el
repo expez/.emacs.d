55a60b7a69fcf6d8e301150cbac8aeb1b1bdeacf
Lars Andersen <lars.andersen@gjensidige.no>
2015-01-20 09:53:02 +0100

    diminish skewer

--- lisp/init-diminish.el
+++ lisp/init-diminish.el
@@ -62,6 +62,9 @@
 (after-load 'git-gutter
   (diminish 'git-gutter-mode))
 
+(after-load 'skewer
+  (diminish 'skewer-mode))
+
 (defmacro rename-modeline (package-name mode new-name)
   `(eval-after-load ,package-name
      (defadvice ,mode (after rename-modeline activate)
