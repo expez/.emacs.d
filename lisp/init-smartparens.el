e673a1d828c9ecd195c16399a3c82f6bb22c934e
Lars Andersen <lars.andersen@gjensidige.no>
2015-02-03 09:29:02 +0100

    fix broken smartparens hook
    would run when keymap was uninitialized

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -11,22 +11,23 @@
                    '(prog-mode nxml-mode sgml-mode)))
 
 (defun my-smartparens-mode-hook ()
-  (eval-after-load 'evil
-    (progn (evil-smartparens-mode 1)
-           (evil-define-key 'normal sp-keymap
-             (kbd "C-t") 'sp-transpose-sexp
-             "[" (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "["))
-             "{" (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "{"))
-             "(" (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "(")))
-           (when evil-normal-state-local-map
-             (fill-keymap evil-normal-state-local-map
-                         "H" 'sp-backward-up-sexp
-                         "L" 'sp-up-sexp
-                         "C-9" 'sp-backward-barf-sexp
-                         "C-0" 'sp-forward-barf-sexp
-                         "M-9" 'sp-backward-slurp-sexp
-                         "M-0" 'sp-forward-slurp-sexp)))))
-(add-hook 'smartparens-enabled-hook #'my-smartparens-mode-hook)
+  (when smartparens-mode
+    (evil-smartparens-mode 1)
+    (evil-define-key 'normal sp-keymap
+      (kbd "C-t") 'sp-transpose-sexp
+      "[" (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "["))
+      "{" (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "{"))
+      "(" (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "(")))
+    (fill-keymap evil-normal-state-local-map
+                 "H" 'sp-backward-up-sexp
+                 "L" 'sp-up-sexp
+                 "C-9" 'sp-backward-barf-sexp
+                 "C-0" 'sp-forward-barf-sexp
+                 "M-9" 'sp-backward-slurp-sexp
+                 "M-0" 'sp-forward-slurp-sexp)))
+;; When this hook was set on smartparens `evil-normal-state-local-map'
+;; would sometimes be nil, causing a most annoying race-condition.
+(add-hook 'evil-local-mode-hook #'my-smartparens-mode-hook)
 
 (sp-with-modes '(markdown-mode
                  rst-mode)
