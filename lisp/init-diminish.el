396e5fafd38dae400e62a7b0a847e3994f474b4c
expez <expez@expez.com>
2015-03-03 07:51:12 +0100

    diminish smartparens
    enough already

--- lisp/init-diminish.el
+++ lisp/init-diminish.el
@@ -74,6 +74,9 @@
 (after-load 'evil-smartparens
   (diminish 'evil-smartparens-mode))
 
+(after-load 'smartparens
+  (diminish 'smartparens-mode))
+
 (defmacro rename-modeline (package-name mode new-name)
   `(eval-after-load ,package-name
      (defadvice ,mode (after rename-modeline activate)
