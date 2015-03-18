391421cc96cad67311993180899136e64cdcdfb0
expez <expez@expez.com>
2015-03-18 16:46:42 +0100

    autofetch PRs with magit

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -37,6 +37,21 @@
     (magit-gh-pulls-mode 1)
     (magit-gh-pulls-reload)))
 
+(defun endless/add-PR-fetch ()
+  "If refs/pull is not defined on a GH repo, define it."
+  (let ((fetch-address
+         "+refs/pull/*/head:refs/pull/origin/*"))
+    (unless (member
+             fetch-address
+             (magit-get-all "remote" "origin" "fetch"))
+      (when (string-match
+             "github" (magit-get "remote" "origin" "url"))
+        (magit-git-string
+         "config" "--add" "remote.origin.fetch"
+         fetch-address)))))
+
+(add-hook 'magit-mode-hook #'endless/add-PR-fetch)
+
 (setq vc-follow-symlinks t)
 
 (add-auto-mode 'gitconfig-mode "gitconfig$")
