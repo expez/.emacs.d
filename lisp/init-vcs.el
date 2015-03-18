527b9d64a36c297f4d23a7672cf4996f306dd8a2
expez <expez@expez.com>
2015-03-18 16:47:57 +0100

    easily create github PRs

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -50,6 +50,22 @@
          "config" "--add" "remote.origin.fetch"
          fetch-address)))))
 
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/compare/%s"
+     (replace-regexp-in-string
+      "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+      (magit-get "remote"
+                 (magit-get-current-remote)
+                 "url"))
+     (magit-get-current-branch))))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "V"
+     #'endless/visit-pull-request-url))
+
 (add-hook 'magit-mode-hook #'endless/add-PR-fetch)
 
 (setq vc-follow-symlinks t)
