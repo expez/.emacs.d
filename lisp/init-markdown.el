efb84c398846ebc03c99583d95d1dc074c48a8ff
expez <expez@expez.com>
2015-03-27 17:52:49 +0100

    gfm for github readme

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -2,14 +2,18 @@
 (require 'markdown-mode)
 
 (add-auto-mode 'markdown-mode "\\.md\\'")
+(add-auto-mode 'gfm-mode "README\\.md\\'")
 
 (fill-keymap markdown-mode-map
-               "C-c C-f" 'nil
-               "C-c C-f C-b" 'markdown-insert-bold
-               "C-c C-f C-<tab>" 'markdown-insert-italic
-               "C-c C-f C-l" 'markdown-insert-link)
+             "C-c C-f" 'nil
+             "C-c C-f C-b" 'markdown-insert-bold
+             "C-c C-f C-<tab>" 'markdown-insert-italic
+             "C-c C-f C-l" 'markdown-insert-link)
 
-(add-lambda 'markdown-mode-hook
+(defun my-markdown-mode-hook ()
+  (change-whitespace-line-column 250)
   (visual-line-mode 1))
+
+(add-hook 'markdown-mode-hook #'my-markdown-mode-hook)
 
 (provide 'init-markdown)
