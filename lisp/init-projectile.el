a4d88987e833afbfcb0da796ecd7c06027e74523
expez <expez@expez.com>
2015-05-04 10:39:04 +0200

    globally ignore more stuff with projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -5,5 +5,5 @@
 (setq projectile-use-git-grep t
       projectile-indexing-method 'alien)
 (setq projectile-globally-ignored-directories
-      (append '("bower_components") projectile-globally-ignored-directories))
+      (append '("bower_components" "node_modules" "build") projectile-globally-ignored-directories))
 (provide 'init-projectile)
