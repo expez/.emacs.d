731ea0c43ca50298dd6481642446c69495b4143c
Lars Andersen <expez@expez.com>
2015-08-28 18:24:33 +0200

    projectile create test files and ignore more dirs

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -4,7 +4,10 @@
 
 (setq projectile-use-git-grep t
       projectile-indexing-method 'alien
-      projectile-enable-caching nil)
+      projectile-enable-caching nil
+      projectile-create-missing-test-files t)
 (setq projectile-globally-ignored-directories
-      (append '("bower_components" "node_modules" "build") projectile-globally-ignored-directories))
+      (append '("bower_components" "node_modules" "build" "out" "target"
+                ".cljs_rhino_repl" ".cljs_nashorn_repl")
+              projectile-globally-ignored-directories))
 (provide 'init-projectile)
