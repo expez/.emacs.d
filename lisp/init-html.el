e2fc014e710cf4b1a17127f43a3bc137ef4d02dc
Lars Andersen <expez@expez.com>
2016-02-23 11:37:42 +0100

    Get rid of some packages I never use

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,26 +1,7 @@
 (require-package 'emmet-mode)
-(require-package 'ac-emmet)
 (require-package 'know-your-http-well)
-(require-package 'web-mode)
-(require-package 'angular-snippets)
 (require 'sgml-mode)
-(require 'web-mode)
 (require 'emmet-mode)
-(require 'angular-snippets)
-
-(defun my-web-mode-hook ()
-  (add-hook 'local-write-file-hooks (lambda () (delete-trailing-whitespace) nil))
-  ;; For some reason the manual says to apply mode settings in a hook.
-  (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-disable-auto-pairing nil
-        web-mode-enable-current-element-highlight t)
-  (emmet-mode 1))
-(add-hook 'web-mode-hook #'my-web-mode-hook)
-(add-auto-mode 'web-mode "\\.mustache")
-(add-auto-mode 'web-mode "\\.hbs")
-(add-auto-mode 'web-mode "\\.erb")
 
 (defun my-html-mode-hook ()
   (change-whitespace-line-column 120)
@@ -31,7 +12,6 @@
 (add-hook 'html-mode-hook #'my-html-mode-hook)
 
 (add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'sgml-mode-hook 'ac-emmet-html-setup)
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
 (eval-after-load "sgml-mode"
   '(define-key html-mode-map (kbd "C-c C-d") 'ng-snip-show-docs-at-point))
@@ -59,10 +39,6 @@ With a prefix it creates an ordered list."
         (while (< (line-number-at-pos) last-line)
           (html-wrap-in-tag "li" (point-at-bol) (point-at-eol))
           (forward-line))))))
-
-(fill-keymaps '(html-mode-map web-mode-map)
-              "C-c b" 'web-beautify-dwim
-              "C-c l" 'html-create-list)
 
 (defadvice sgml-delete-tag (after reindent activate)
   (indent-region (point-min) (point-max)))
