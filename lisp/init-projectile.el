7168c473059862bcc9a064f77e57f950e03a8a61
Lars Andersen <expez@expez.com>
2020-10-15 16:00:26 +0200

    Refactor projectile settings slightly

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -5,8 +5,8 @@
 (setq projectile-use-git-grep t
       projectile-indexing-method 'alien
       projectile-enable-caching nil
-      projectile-create-missing-test-files t)
-(setq projectile-globally-ignored-directories
+      projectile-create-missing-test-files t
+      projectile-globally-ignored-directories
       (append '("bower_components" "node_modules" "build" "out" "target"
                 ".cljs_rhino_repl" ".cljs_nashorn_repl" ".nashorn_code_cache")
               projectile-globally-ignored-directories))
